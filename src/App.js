e6a3f6b8a876d2f8e961f301cd6c006fcc3fc5bc
Kim Blanck <kimblanck@Kims-Air.home>
2021-03-04 12:11:51 -0500

    images on App.js page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import grapes from './assets/imgs/grapes.png';
+import banana from './assets/imgs/banana.png';
+import mango from './assets/imgs/mango.png';
+import orange from './assets/imgs/orange.png';
+import bowl from './assets/imgs/bowl.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Fruit Bowl forthcoming!
-        <h6>
-        © Group 9 Productions, LLC
-        </h6>
+        fruitbowl
+        <div className="fruitbowl">
+          <img src={grapes} />
+          <img src={banana} />
+          <img src={mango} />
+          <img src={orange} />
+          {/* <img src={bowl} /> */}
+        </div>
       </header>
       </div>
   );
