1308bc4df0e37fb3ba3d05dc5e45948c348dde9a
Kim Blanck <kimblanck@Kims-Air.home>
2021-03-03 21:52:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Fruit Bowl forthcoming!
+      </header>
+    </div>
+  );
+}
+
+export default App;
