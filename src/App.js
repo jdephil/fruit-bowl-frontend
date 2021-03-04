e88249add7d421b5314105a2d335d36d93a167c9
Kim Blanck <kimblanck@Kims-Air.home>
2021-03-04 18:21:13 -0500

    try this show page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
 import Fruitbowl from './Fruitbowl'
 import TryThis from './TryThis'
 
@@ -7,14 +7,13 @@ import TryThis from './TryThis'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-            <Route path='/' component={Fruitbowl} />
-            <Route exact path='/trythis' component={TryThis} />
-        </Switch>
-      </BrowserRouter>
-      
-      </div>
+      <main>
+        {/* <Switch> */}
+            <Route path='/' exact component={Fruitbowl} />
+            <Route path='/trythis' component={TryThis} />
+        {/* </Switch> */}
+      </main>
+    </div>
   );
 }
 
--- src/TryThis.js
+++ src/TryThis.js
@@ -1,11 +1,34 @@
-// import axios from 'axios'
+import axios from 'axios'
+import React from 'react'
+import './App.css'
 
-function TryThis() {
-  return (
-        <div className="tryThis">
-         <h1>hi</h1>
+class TryThis extends React.Component {
+  state = {
+    name: 'Test test test',
+    category: '',
+    url: '',
+    urlTitle: ''
+  }
+  componentDidMount = () => {
+    axios.get('/').then((res) => {
+      this.setState({
+        tips: res.data
+      })
+    })
+  }
+  render = () => {
+    return (
+      <div className="tryThisContainer">
+        <a href="/" id="goBack">&#60; Back to Fruit Bowl</a>
+        <h1 className="tryThisName">Try this: {this.state.name}</h1>
+        {/* <img src for fruit /> */}
+        <div className="learnMore">
+          Learn more <span id="learnMoreArrow">&#62;</span>
         </div>
-  );
+        <a href="#" id="tryAgain">Not interested? Try again!</a>
+      </div>
+    )
+  }
 }
 
 export default TryThis;
