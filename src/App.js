8eb71fc86f1708c3708b8a8be005dc3f825dfbd0
jdephil <jennifer.dephillips@gmail.com>
2021-03-05 10:37:06 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
-
-
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import Fruitbowl from './Fruitbowl'
 import TryThis from './TryThis'
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <BrowserRouter>
         <Switch>
            <Route exact path='/' component={Fruitbowl} />
            <Route exact path='/trythis' component={TryThis} />
         </Switch>
+      </BrowserRouter>
     </div>
   );
 }
