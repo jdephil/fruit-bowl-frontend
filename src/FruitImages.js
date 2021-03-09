7dfb1eeaff7748f212c33793c65d934e06e04d06
jdephil <jennifer.dephillips@gmail.com>
2021-03-09 01:10:51 -0500

    mapped object

--- src/FruitImages.js
+++ src/FruitImages.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import TipsData from './TipsData'
 
 
 
@@ -8,27 +9,28 @@ function FruitImages(props) {
   const [categoryDiv, setCategoryDiv] = useState('')
   const [tipsByCategory, setTipsByCategory] = useState('')
   const [tipsData, setTipsData] = useState({
-    // title: '', 
-    // category: '',
-    // url: '',
-    // urlTitle: ''
+    title: '', 
+    category: '',
+    url: '',
+    urlTitle: ''
   })
-
+  
   useEffect(() => {
     axios.get('http://fruitbowl-backend.herokuapp.com/api/tips')
-      .then(res => {
-        const allTips = res.data
-       
+    .then(res => {
+      res.data.map(tip => {
+        let tipTitle = tip.title
         setTipsData({
-          // title: res.data[].title, 
-          // category: res.data.category, 
-          // url: res.data.url, 
-          // urlTitle: res.data.urlTitle
-          allTips
-        })
-        
+          title: tipTitle, 
+          category: tip.category, 
+          url: tip.url, 
+          urlTitle: tip.urlTitle
       })
-    }, [])
+        return("return")
+      
+      })
+  })
+})
     
     let clickHandler = (e) => {
       if (e.target.src.includes('/grapes.png')) {
@@ -44,16 +46,10 @@ function FruitImages(props) {
     
     return (
       <div>
-    <div className='category'>
-     {tipsData.map((tip, i) => {
-        return (
-          <Link tip={tip} to='/trythis'>{categoryDiv}</Link>
-        )
-      })
-    }
-        
-      </div>
-    <div className="fruitbowl">
+        <div className='category'>
+          <Link tipsData={tipsData} to='/trythis'>{categoryDiv}</Link>
+        </div>
+      <div className="fruitbowl">
       
       <div className="fruitLinksDiv">
           <div className='fruitLink grapesImg'><img className='fruitImg' onClick={clickHandler} src='/grapes.png' alt='clickable grapes' /></div>
