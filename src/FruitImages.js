13da995fa46b573217b11283c75621b73f993325
jdephil <jennifer.dephillips@gmail.com>
2021-03-08 22:47:52 -0500

    axios call working

--- src/FruitImages.js
+++ src/FruitImages.js
@@ -1,47 +1,75 @@
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
-// import axios from 'axios'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 
 function FruitImages(props) {
-  const [category, setCategory] = useState('')
-  const allTips = props.tips.map((tip, i) => {
-    if (tip.category === "Mental Health") {
-      return tip
-    } else if (tip.category === "Relational Health") {
-      return tip
-    } else {
-      console.log('done')
-    }
+  const [categoryDiv, setCategoryDiv] = useState('')
+  const [tipsByCategory, setTipsByCategory] = useState('')
+  const [tipsData, setTipsData] = useState({
+    // title: '', 
+    // category: '',
+    // url: '',
+    // urlTitle: ''
   })
- 
-  let hoverInHandler = (e) => {
-    setCategory(e.target.cat)
-    console.log(e.target.src)
-    console.log("hover in")
-  }
 
-  let clickHandler = () => {
-    console.log("hover out")
-  }
-
-  return (
-    <div className="fruitbowl">
-      <div className='category'>
-        {category}
-      </div>
-      <div className="fruitLinksDiv">
-          <Link className='fruitLink grapesImg' to='/trythis'><img className='fruitImg' onClick={clickHandler} src='/grapes.png' alt='clickable grapes' /></Link>
-          <Link className='fruitLink bananaImg' to='/trythis'><img className='fruitImg' src='/banana.png' alt='clickable bananas'/></Link>
-          <Link className='fruitLink mangoImg' to='/trythis'><img className='fruitImg' src='/mango.png' alt='clickable mango'/></Link>
-          <Link className='fruitLink orangeImg' to='/trythis'><img className='fruitImg' src='/orange.png' alt='clickable orange'/></Link>
-          <img className='bowlImg' src='/bowl.png' alt='bowl' />
-      </div>
-      <div className='bowlDiv'>
+  useEffect(() => {
+    axios.get('http://fruitbowl-backend.herokuapp.com/api/tips')
+      .then(res => {
+        const allTips = res.data
+       
+        setTipsData({
+          // title: res.data[].title, 
+          // category: res.data.category, 
+          // url: res.data.url, 
+          // urlTitle: res.data.urlTitle
+          allTips
+        })
+        
+      })
+    }, [])
+    
+    let clickHandler = (e) => {
+      if (e.target.src.includes('/grapes.png')) {
+        setCategoryDiv('Mental Health')
+      } else if (e.target.src.includes('/banana.png')) {
+        setCategoryDiv('Relational Health')
+      } else if (e.target.src.includes('/mango.png')) {
+        setCategoryDiv('Nutrition')
+      } else if (e.target.src.includes('/orange.png')) {
+        setCategoryDiv('Physical Fitness')
+      }
+    }
+    
+    return (
+      <div>
+    <div className='category'>
+     {tipsData.map((tip, i) => {
+        return (
+          <Link tip={tip} to='/trythis'>{categoryDiv}</Link>
+        )
+      })
+    }
         
       </div>
-    </div>
+    <div className="fruitbowl">
+      
+      <div className="fruitLinksDiv">
+          <div className='fruitLink grapesImg'><img className='fruitImg' onClick={clickHandler} src='/grapes.png' alt='clickable grapes' /></div>
+          <div className='fruitLink bananaImg' ><img className='fruitImg' onClick={clickHandler} src='/banana.png' alt='clickable bananas'/></div>
+          <div className='fruitLink mangoImg' ><img className='fruitImg' onClick={clickHandler} src='/mango.png' alt='clickable mango'/></div>
+          <div className='fruitLink orangeImg' ><img className='fruitImg' onClick={clickHandler} src='/orange.png' alt='clickable orange'/></div>
+          
+      </div>
+      <div className='bowlDiv'>
+        <img className='bowlImg' src='/bowl.png' alt='bowl' />
+      </div>
+      <div>
 
+      </div>
+    </div>
+    </div>
   );
 }
 
