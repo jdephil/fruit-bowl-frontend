347a58aff66215e244a4f4bd045c1223a51796cd
Kim Blanck <kimblanck@Kims-MacBook-Air.local>
2021-03-09 13:27:44 -0500

    try this commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-<<<<<<< HEAD
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-=======
 
 
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
->>>>>>> a38b80f70579f4a07f3d5fbf462a535406bcc4e4
 import Fruitbowl from './Fruitbowl'
 import TryThis from './TryThis'
 
@@ -17,16 +13,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-<<<<<<< HEAD
-            <Route exact path='/' component={Fruitbowl} />
-            <Route exact path='/trythis' component={TryThis} />
-        </Switch>
-        </BrowserRouter>
-=======
            <Route exact path='/' component={Fruitbowl} />
            <Route exact path='/trythis' component={TryThis} />
         </Switch>
->>>>>>> a38b80f70579f4a07f3d5fbf462a535406bcc4e4
+      </BrowserRouter>
     </div>
   );
 }
