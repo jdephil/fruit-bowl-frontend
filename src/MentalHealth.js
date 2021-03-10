df2e84ae5febc3c88e163ac63861956d4aaf5100
jdephil <jennifer.dephillips@gmail.com>
2021-03-10 12:11:50 -0500

    accessed single tip

--- src/MentalHealth.js
+++ src/MentalHealth.js
@@ -4,42 +4,55 @@ import {Link} from 'react-router-dom';
 import './App.css'
 
 function MentalHealth() {
-    useEffect(() => {
-        fetchTips();
-    }, []);
-    // const [tips, setTips] = useState([
-    //     {
-    //         category: 'Mental Health'
-    //     }
-    // ]);
-    // const fetchTips = async () => {
+    const [tips, setTips] = useState([
+           
+            ]);
+            // const fetchTips = async () => {
     //     const data = await fetch('http://fruitbowl-backend.herokuapp.com/api/tips');
     //     const tips = await data.json();
     //     setTips(tips)
     // }
+    const [tipP, setTipP] = useState([])
     let newArr = []
-    const fetchTips = () => {
+    
+    useEffect(() => {
+        fetchTips()
+        
+        
+    }, []);
+    const fetchTips = (callback) => {
         axios.get('http://fruitbowl-backend.herokuapp.com/api/tips')
-          .then(function (response) {
+        .then(function (response) {
               console.log(response.data)
               const result = response.data.map(function(tip) {
-                  if (tip.category === 'Mental Health') {
-                      newArr.push({"title": tip.title})
-                  }
-              })
-          })
+                  //   if (tip.category === 'Mental Health') {
+                //       newArr.push({title: tip.title})
+                //   }
+               
+                    newArr.push(tip)
+                    
+                    
+                    
+            })
+        }).then (() => {
+            
+            setTipP(newArr[1].title)
+        })
     }
-    console.log(newArr)
+    console.log(newArr[1])
     const randomTitle = () => {
         let randomIndex = Math.floor(Math.random() * newArr.length)
         let tryThisTitle = newArr[0]
         console.log(tryThisTitle)
     }
     randomTitle();
+    if (!tipP) {
+        return (<p>"loading"</p>)
+    } 
     return (
         <div className="tryThisContainer">
           <a href="/" id="goBack">&#60; Back to Fruit Bowl</a>
-            <h1 className="tryThisName">Try this: {}</h1>
+            <h1 className="tryThisName">Try this: {tipP}</h1>
             <Link>
                 <div className="learnMore">
                     Learn more <span id="learnMoreArrow">&#62;</span>
